06b859dae1e6cb54f63163a20f8f60775cf817a8
Rodney "XMeintek" Lee <49397250+JustRodneyLee@users.noreply.github.com>
2019-09-03 07:42:24 +0800

    File Update

---
+++ OCR_Main.py
@@ -0,0 +1,28 @@
+from scipy import misc
+from matplotlib.pyplot import imread
+import numpy as np
+
+def preprocess(self, src):
+    name = src.split('\\').
+    img = imread()
+
+class ForwardLayer:
+    def __init__(self):
+        
+    def 
+
+def main():
+    hiddenLayers = []
+    hiddenLayers.append(ForwardLayer())
+    if os.path.isfile("OCR_en_US_weights.csv") & os.path.isfile("OCR_en_US_biases.csv"):
+        hiddenLayers[0].load(np.loadtxt("OCR_en_US_weights.csv",delimiter=","),np.loadtxt("OCR_en_US_biases.csv",delimiter=","))
+    while True:
+        print("Enter path of image:")
+        path = input()
+        #preprocess the image of that path
+        #train through the neural network
+        #print result
+        
+
+if __name__ == "__main__":
+    main()
---
+++ preprocess.py
@@ -0,0 +1,24 @@
+from scipy import misc
+from matplotlib.pyplot import imread
+import numpy as np
+
+def main(src, dst):
+    with open(src, 'r') as f:
+        list = f.readlines()
+    data = []
+    labels = []
+    for i in list:
+        name, label = i.strip('\n').split(' ')
+        print(name + 'processed')
+        img = imread(name)
+        #img = img/255
+        img.resize((img.size, 1))
+        data.append(img)
+        labels.append(int(label))
+
+    print('write to npy')
+    np.save(dst, [data,labels])
+    print('completed')
+
+if __name__ == "__main__":
+    main("validate.txt","validate.npy")
