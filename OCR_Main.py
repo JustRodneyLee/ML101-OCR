9a9f45d38e13dc26bec7e1630dd271947d6c27fc
Rodney "XMeintek" Lee <49397250+JustRodneyLee@users.noreply.github.com>
2019-09-08 12:08:56 +0800

    Add files via upload

--- OCR_Main.py
+++ OCR_Main.py
@@ -1,28 +1,76 @@
 from scipy import misc
 from matplotlib.pyplot import imread
 import numpy as np
+import os
 
-def preprocess(self, src):
-    name = src.split('\\').
-    img = imread()
+def ConvertToArray(src):        
+    img = imread(src)
+    #img = img/255
+    #black and white issue...
+    img.resize((289, 1))    
+    return img        
 
-class ForwardLayer:
-    def __init__(self):
+class FullyConnectedLayer:
+    def __init__(self,l_x,l_y):
+        self.weights = np.random.randn(l_y, l_x) / np.sqrt(l_x)
+        self.bias = np.random.randn(l_y, 1)
         
-    def 
+    def load(self, weights, biases):
+        self.weights = weights
+        self.biases = biases
+
+    def forward(self, x):
+        self.x = x
+        self.y = np.array([np.dot(self.weights,data) + self.bias for data in x])
+        return self.y  
+
+class ActivationLayer:
+    def __init__(self, actType):
+        self.activation = actType
+    
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
+
+    def relu(self, x):
+        i = 0
+        out = np.zeros_like(x)
+        for xx in x:
+            if np.sum(xx)>0:
+                out[i]=xx
+            i+=1
+        return out
+    
+    def forward(self, x):
+        self.x = x
+        if self.activation=="Sigmoid":            
+            self.y = self.sigmoid(x)
+        elif self.activation=="RELU":            
+            self.y = self.relu(x)
+        return self.y
 
 def main():
+    print("Loading OCR...")
     hiddenLayers = []
-    hiddenLayers.append(ForwardLayer())
-    if os.path.isfile("OCR_en_US_weights.csv") & os.path.isfile("OCR_en_US_biases.csv"):
-        hiddenLayers[0].load(np.loadtxt("OCR_en_US_weights.csv",delimiter=","),np.loadtxt("OCR_en_US_biases.csv",delimiter=","))
+    hiddenLayers.append(FullyConnectedLayer(17*17, 26))
+    hiddenLayers[0].load(np.loadtxt("OCR_en_US_weights.csv",delimiter=","),np.loadtxt("OCR_en_US_biases.csv",delimiter=","))
+    hiddenLayers.append(ActivationLayer("Sigmoid"))
+    #hiddenLayers : FullyConnectedLayer->ActivationLayer
+    print("Loading Complete!")
     while True:
         print("Enter path of image:")
         path = input()
-        #preprocess the image of that path
-        #train through the neural network
-        #print result
-        
-
+        if (path=="bye" or path=="quit" or path=="exit"):
+            break
+        dat = ConvertToArray(path)
+        x = []
+        for i in range(1, 26):
+            x.append(dat)
+    
+        for layer in hiddenLayers:
+            x = layer.forward(x)
+    
+        #ASCII A 65 Z 90
+        print(chr(np.argmax(hiddenLayers[1].y)+65))
+    
 if __name__ == "__main__":
     main()
